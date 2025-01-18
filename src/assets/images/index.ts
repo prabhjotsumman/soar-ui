e1f640a66d6290765394dc3079479b2f7ce87a55
prabhjotsumman <prabhjot.nith@gmail.com>
2025-01-18 16:18:15 -0700

    images default export

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+export { default as profilePic } from "./profile-pic.jpeg";
+export { default as user1 } from "./users/user1.png";
+export { default as user2 } from "./users/user2.png";
+export { default as user3 } from "./users/user3.png";
+export { default as user4 } from "./users/user4.png";
+export { default as user5 } from "./users/user5.png";
+export { default as user6 } from "./users/user6.png";
