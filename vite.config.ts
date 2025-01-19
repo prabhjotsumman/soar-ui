e44b3520c016658ae8b4157dac0e9f7da550ff9a
prabhjotsumman <prabhjot.nith@gmail.com>
2025-01-19 13:23:34 -0700

    vite.config.ts updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-// import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // svgLoader(), // Ensures inline SVGs are processed,
     svgr(),
   ],
+  base: '/',
 });
